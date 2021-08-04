0ffb3777b2e4b95f9e06d6f7c94976be49cd9ffb
Mehmet <mehmetkusken@gmail.com>
2021-08-04 13:36:52 -0600

    commit tree aa374acb51e4a73434dc4d420c6983b914de18d5

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-/* Given Code, don't edit */
-
-document.querySelector("button").addEventListener("click", handleClick);
-
+import { endianness } from "os"
 function handleClick(e) {
   const timeString = document.getElementById('time').value
   displayMessage(greet(timeString))
@@ -9,5 +6,19 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
+function greet(time) {
+  let splitNums = time.split(':')
+  let hour = parseInt(splitNums[0])
+  if(hour < 12) {
+    return 'Good Morning';}
+  else if(hour > 12 && hour < 17){
+    return 'Good Afternoon';}
+  else {
+    return 'Good Evening';}
+}
+
+
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+  document.getElementById('greeting').innerText = string;
+}
